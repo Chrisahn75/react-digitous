616e25d1346a1de373b94110c79452e5f460e1d3
Chrisahn75 <chris.ahona@gmail.com>
2022-01-24 16:20:25 +0100

    step 5

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import './App.css';
+import Steps from "./components/Steps"
+import "./styles/global.css"
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 class App extends React.Component {
+tempMin = -20;
+tempMax = 40;
+heartMin = 80;
+heartMax = 180;
+stepsMin = 0;
+stepsMax = 50000;
   render() {
     return (
-      <div>
-        <h1 className="text-primary">Hello !</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <p> Temperature : {this.tempMin}</p>
+          <p> Heart: {this.heartMin}</p>
+          <p> Steps : {this.stepsMin}</p>
+          <Steps />
+        </div>
       </div>
     );
   }
